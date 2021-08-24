bc9384231ff8bd4847a5c4ebae6d64d5e211fb52
kiraahmad <60839257+kiraahmad@users.noreply.github.com>
2021-08-24 12:22:23 +0800

    init + Styles + Header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,17 @@
-
+import { Container } from 'react-bootstrap';
+import Header from './components/Header'
+import Footer from './components/Footer'
 
 function App() {
   return (
     <>
-    <h1> Welcome To SalShop</h1>
+    <Header />
+    <main className='py-3'>
+      <Container>
+      <h1> Welcome To SalShops</h1>
+      </Container>
+    </main>
+    <Footer />
     </>
   );
 }
