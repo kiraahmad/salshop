ac230317246d4deaff780751f3cd8df6abb310b7
kiraahmad <60839257+kiraahmad@users.noreply.github.com>
2021-08-26 12:14:41 +0800

    Redux to home screen, products from DB

--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -1,8 +1,9 @@
+
 export const productListRedcuer = (state={ products: [] }, action) => {
     switch (action.type) {
         case 'PRODUCT_LIST_REQUEST': 
             return { loading: true, products: [] }
-        case 'PRODUCT_LIST-SUCCESS':
+        case 'PRODUCT_LIST_SUCCESS':
             return {loading: false, products: action.payload}
         case 'PRODUCT_LIST_FAIL':
             return { loading: false, error: action.payload }
